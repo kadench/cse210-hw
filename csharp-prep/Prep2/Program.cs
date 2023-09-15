adf080c1943dc5812f8540e2c4c74150ccc4b3f1
kadench <kadench@byui.edu>
2023-09-14 23:26:51 -0600

    Finished Changes
    Fixed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Data.Common;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade value: ");
+        string kh_grade_from_user = Console.ReadLine();
+        int kh_grade_value_as_int = int.Parse(kh_grade_from_user);
+
+        string kh_grade = "";
+
+        if (kh_grade_value_as_int >= 90)
+        {
+            if (kh_grade_value_as_int >= 97)
+            {
+                kh_grade = "A+";
+            }
+            else if (kh_grade_value_as_int <= 93)
+            {
+                kh_grade = "A-";
+            }
+            Console.WriteLine($"You have an {kh_grade}.");
+        }
+        else if (kh_grade_value_as_int >= 80)
+        {
+            if (kh_grade_value_as_int >= 87)
+            {
+                kh_grade = "B+";
+            }
+            else if (kh_grade_value_as_int <= 83)
+            {
+                kh_grade = "B-";
+            }
+            Console.WriteLine($"You have an {kh_grade}.");
+        }
+        else if (kh_grade_value_as_int >= 70)
+        {
+            if (kh_grade_value_as_int >= 77)
+            {
+                kh_grade = "C+";
+            }
+            else if (kh_grade_value_as_int <= 73)
+            {
+                kh_grade = "C-";
+            }
+            Console.WriteLine($"You have an {kh_grade}.");
+        }
+        else if (kh_grade_value_as_int >= 60)
+        {
+            if (kh_grade_value_as_int >= 67)
+            {
+                kh_grade = "D+";
+            }
+            else if (kh_grade_value_as_int <= 63)
+            {
+                kh_grade = "D-";
+            }
+            Console.WriteLine($"You have an {kh_grade}.");
+        }
+        else if (kh_grade_value_as_int < 60)
+        {
+            Console.WriteLine("You have an F.");
+        }
     }
 }
