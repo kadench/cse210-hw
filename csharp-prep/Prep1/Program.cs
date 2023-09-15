749f88098f70b805952ce748bcbf7a79bd8f70df
kadench <kadench@byui.edu>
2023-09-14 22:47:05 -0600

    C# Prep 1—Variables and Input/Output
    Part 1 Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name?: ");
         string kh_first_name = Console.ReadLine();
         Console.Write("What is your last name?: ");
-        string k_last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {k_last_name}, {kh_first_name} {k_last_name}");
+        string kh_last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {kh_last_name}, {kh_first_name} {kh_last_name}");
     }
 }
