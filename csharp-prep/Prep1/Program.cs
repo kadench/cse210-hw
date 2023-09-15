c93b64bb20793d8534925682e173f6e3a191d855
kadench <kadench@byui.edu>
2023-09-14 22:44:02 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?: ");
+        string kh_first_name = Console.ReadLine();
+        Console.Write("What is your last name?: ");
+        string kh_last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {kh_last_name}, {kh_first_name} {kh_last_name}");
     }
 }
