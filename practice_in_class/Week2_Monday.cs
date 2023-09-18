202c856244ae2c317edb65da4dca6f5f6cf67ff2
kadench <144969637+kadench@users.noreply.github.com>
2023-09-18 12:28:45 -0600

    Created a Folder for in-class use
    Will delete when I get home.

---
+++ practice_in_class/Week2_Monday.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.Write("What is your favorite number?: ");
+      string kh_favorite_number_string = Console.ReadLine();
+      int kh_favorite_number = int.Parse(kh_favorite_number_string);
+
+      Console.Write("What is your first name?: ")
+      string kh_first_name = Console.ReadLine();
+      Console.Write("What is your last name?: ")
+      string kh_last_name = Console.ReadLine();
+
+      Console.Writeline($"Your name is: {kh_first_name} {kh_last_name}.");
+      Console.Writeline($"Your favorite number is: {kh_favorite_number}");
+
+    }
+}
