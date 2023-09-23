aaf0138ee8d95a97fe49e9988561700795f6c877
kadench <kadench@byui.edu>
2023-09-23 12:13:35 -0600

    Fixed Prep 3 issue and updated index.html to match.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,21 +9,20 @@ class Program
         Console.WriteLine("Enter your guesses below.");
         Console.WriteLine("");
 
-        // Making a randomly generated number for the user to guess. (1-25).
+        // Making a randomly generated number for the user to guess. (1-303).
         Random kh_randomGenerator = new Random();
-        int kh_magic_number = kh_randomGenerator.Next(1, 25);
+        int kh_magic_number = kh_randomGenerator.Next(1, 303);
 
         int kh_guessed_number = -1;
         
         // Making a while loop to do these actions below until the user guessed number equals the randomly generated one.
-        // (Took forever to figure this one out because I was trying to use a do while loop. I need more clarification on those if we are ever to use them.)
         while (kh_guessed_number != kh_magic_number)
         {
             // Asking the user for a guess.
             Console.Write("What is your guess?: ");
 
             // Putting the guessed number into a variable and turning it into an int.
-            int kh_guessed_number = int.Parse(Console.ReadLine());
+            kh_guessed_number = int.Parse(Console.ReadLine());
             
             // Giving the user feedback to tell if the number they guessed is lower or higher than the magic number.
             if (kh_guessed_number < kh_magic_number)
@@ -39,9 +38,5 @@ class Program
                 Console.WriteLine("You've guessed it!");
             }
         }
-
-
-
-
     }
 }
