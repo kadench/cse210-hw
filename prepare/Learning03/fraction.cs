b6258277227cc07b2a2bd478299e53054adb30fa
kadench <kadench@byui.edu>
2023-10-12 02:07:02 -0600

    Fixed a mistake.

--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -5,21 +5,18 @@ class Fraction
     private int _numerator;
     private int _denominator;
 
-    // Constructor that initializes the number to 1/1
     public Fraction()
     {
         _numerator = 1;
         _denominator = 1;
     }
 
-    // Constructor that takes one parameter for the top and initializes the denominator to 1
     public Fraction(int kh_wholeNumber)
     {
         _numerator = kh_wholeNumber;
         _denominator = 1;
     }
 
-    // Constructor that takes two parameters for the top and the bottom
     public Fraction(int kh_top, int kh_bottom)
     {
         _numerator = kh_top;
